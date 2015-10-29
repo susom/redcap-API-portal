80646413935a67c54cd43589cc6b15716559f7e9
Irvin Szeto <irvins@stanford.edu>
2015-10-29 10:42:03 -0700

    deleteing test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* TEST FILE FOR INCLUDES */
-require_once('models/config.php');
-echo "TEST START<hr><pre>";	
-	
-$path = dirname(__FILE__);
-echo "\n path: $path";
-echo "\n PORTAL_INC_PATH: " . PORTAL_INC_PATH;
-?>

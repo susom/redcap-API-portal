ef3912cf8fd5215d48cf9f53235cbabce8738780
Irvin Szeto <irvins@stanford.edu>
2016-01-26 14:31:52 -0800

    get the weather api working again

--- dashboard/inc/gl_foot.php
+++ dashboard/inc/gl_foot.php
@@ -1,7 +1,7 @@
   <!-- Bootstrap -->
   <script src="js/bootstrap.js"></script>
   <!-- App -->
-  <script src="js/app.js"></script>  
+  <script src="js/app.js"></script>
   <script src="js/slimscroll/jquery.slimscroll.min.js"></script>
   <script src="js/charts/easypiechart/jquery.easy-pie-chart.js"></script>
   <script src="js/charts/sparkline/jquery.sparkline.min.js"></script>
@@ -16,9 +16,8 @@
   <script src="js/calendar/demo.js"></script>
   <script src="js/sortable/jquery.sortable.js"></script>
   <script src="js/app.plugin.js"></script>
-  
   <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-  <script src="js/jquery.simpleWeather.min.js"></script>
+  <script src="js/jquery.simpleWeather.js"></script>
   <script src="js/weather.js"></script>
 </body>
 </html>
@@ -29,7 +28,6 @@ $(document).on('click', function(event) {
       $(".alert").remove();
     });
   }
-    
 });
 
 $("a.disabled").click(function(){

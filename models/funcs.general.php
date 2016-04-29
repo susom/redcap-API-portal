aebb321e3993af930d5a0dd1c1aaf3f763b753b7
Irvin Szeto <irvins@stanford.edu>
2016-04-29 14:55:22 -0700

    add array_column fucntionality

--- models/funcs.general.php
+++ models/funcs.general.php
@@ -1,4 +1,31 @@
 <?php
+if (! function_exists('array_column')) {
+    function array_column(array $input, $columnKey, $indexKey = null) {
+        $array = array();
+        foreach ($input as $value) {
+            if ( ! isset($value[$columnKey])) {
+                trigger_error("Key \"$columnKey\" does not exist in array");
+                return false;
+            }
+            if (is_null($indexKey)) {
+                $array[] = $value[$columnKey];
+            }
+            else {
+                if ( ! isset($value[$indexKey])) {
+                    trigger_error("Key \"$indexKey\" does not exist in array");
+                    return false;
+                }
+                if ( ! is_scalar($value[$indexKey])) {
+                    trigger_error("Key \"$indexKey\" does not contain scalar value");
+                    return false;
+                }
+                $array[$value[$indexKey]] = $value[$columnKey];
+            }
+        }
+        return $array;
+    }
+}
+
 //------------------------------------------------------------------
 /* SESSION FUNCTIONS */
 //------------------------------------------------------------------

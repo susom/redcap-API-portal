c2751d29b2fdf1fb21bfb40a85862d58731a6f95
Irvin Szeto <irvins@stanford.edu>
2016-05-05 10:28:27 -0700

    no print debug to screen

--- models/class.RCAPI.php
+++ models/class.RCAPI.php
@@ -18,7 +18,7 @@ class RC {
 
 		logIt(json_encode($params,1),"DEBUG");
 		$result = self::http_post($api_url, $params);
-		logIt('call API Raw result: ' . print_r($params), "DEBUG");	
+		logIt('call API Raw result: ' . print_r($params, true), "DEBUG");	
 
 		if ($json_decode) {
 			$result = json_decode($result, true);

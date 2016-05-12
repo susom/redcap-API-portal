3b0de4dab1ebb6c07181132ce636c8346d601401
Irvin Szeto <irvins@stanford.edu>
2016-05-12 13:44:49 -0700

    woops

--- models/config.php
+++ models/config.php
@@ -35,7 +35,7 @@ session_start();
 $loggedInUser = getSessionUser($_CFG->SESSION_NAME);
 if( !empty($loggedInUser) ){
 	// Check for logout
-	unset($_SESSION);
+	unset($_SESSION[SESSION_NAME]);
 	if ( isset($_GET['logout']) && $_GET['logout'] == 1 ){
 		logout("Goodbye!");
 	}

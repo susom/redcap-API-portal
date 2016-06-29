a3e55a4022f2272d6a21e2caab07e3b5ce00914a
Irvin Szeto <irvins@stanford.edu>
2016-06-29 14:17:57 -0700

    revert mask back

--- dashboard/inc/classes/Survey.php
+++ dashboard/inc/classes/Survey.php
@@ -425,8 +425,7 @@ class Survey {
         }
 
         if(isset($mask)){  
-          $mask_js[]    = "$(\"#$field_name\").attr(\"placeholder\",\"$placeholder\")";
-          //.attr(\"type\",\"number\");\n";
+          $mask_js[]    = "$(\"#$field_name\").attr(\"placeholder\",\"$placeholder\").attr(\"type\",\"number\");\n";
           // $mask_js[]    = "$(\"#$field_name\").mask(\"$mask\",{placeholder:\"$placeholder\"});\n";
         }
 

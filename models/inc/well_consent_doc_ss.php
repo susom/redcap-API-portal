07ee753346ed586ea6c91b2dee056bb97fc94e48
Irvin Szeto <irvins@stanford.edu>
2016-07-07 12:11:26 -0700

    updated consent

--- models/inc/well_consent_doc_ss.php
+++ models/inc/well_consent_doc_ss.php
@@ -1,19 +1,21 @@
 <section>
 	<h2>PURPOSE OF RESEARCH</h2>
-	<p>The WELL for Life Initiative aims to improve understanding of living well and ways to increase wellness. The WELL for Life Initiative starts with your completion of a survey (the WELL for Life Registry).  In addition, you will be invited to participate in an extension of the registry to learn how biological factors influence living well.  You will also have the opportunity to participate in intervention studies focused on promoting health and wellness. The choice to take part and continue taking part is yours.</p> 
-	<p>You are currently being invited to complete the WELL for Life Registry survey. 
-If you decide to end your participation in this study, you should notify Sandra Winter at 650-723-8513.</p>
-	<p>The WELL for Life Initiative has been created by the Stanford Prevention Research Center in the Stanford University School of Medicine. It will initially target 10, 000 people living in the San Francisco Bay Area, California, with a longer-term goal of including participants from the rest of the United States. WELL for Life Registry provides an ongoing source of valuable information on factors related to health and wellness.</p> 
+	<p>The WELL for Life Initiative aims to identify factors that contribute to wellbeing and health. The WELL for Life Initiative has been created by the Stanford Prevention Research Center in the Stanford University School of Medicine.</p> 
+	<p>You are being invited to enter the WELL for Life Registry, which is gathering information on people’s health and wellbeing. As a WELL for Life Registry participant you will be invited to take part in future WELL for Life Projects. Our goal is to enroll 10,000 people living in the San Francisco Bay Area. In the future, we hope to enroll participants from all across the US.</p>
+	<p>Your participation in this study is voluntary. If you decide to end your participation in this study, you should notify Sandra Winter at 650-723-8513.</p> 
 </section>
 
 <section>
 	<h2>PROGRAM ACTIVITIES</h2> 
-	<p>You are invited to participate in the WELL for Life Registry, which is comprised of a number of surveys – the first wellbeing survey will take about 20-30 minutes and will include information such as your contact details, demographics (for example, age, gender, language), lifestyle habits and behaviors, and your community. You have the right to refuse to answer particular questions.  In the future you will be asked to participate in additional surveys that gather further information about your life, health, and wellness. For example, you will be asked questions about your diet, physical activity and functional capacity.  You may also be asked to provide a photograph, voice or video recording that captures various information about wellbeing.</p>
+	<p>The WELL for Life Registry consists of a number of online surveys. The first survey takes about 20-30 minutes and asks for your contact details and about your basic demographics (e.g., age, gender, language), lifestyle habits and behaviors (e.g., diet, physical activity, tobacco use), and your community. Later surveys will ask similar questions about your lifestyle habits, health, and wellness. You also may be asked to provide a photograph, voice or video recording as a measure of wellbeing. You have the right to refuse to answer particular questions.</p>
+	<p>We will test different strategies to make participation in the WELL for Life Registry engaging. For example, we may “gamify” the survey, provide you with varying types of feedback, change the order in which you answer the surveys or the messages and reminders that we send you, and provide you with informational content.</p>
+	<p>You may be asked if you would be interested in being interviewed about your experience as a WELL for Life participant. If you do NOT wish to be contacted about being interviewed, please indicate that on the first survey.</p>
+	<p>You may also be asked if you would be interested in being contacted about participating in other non-WELL related studies in the future.  If you do NOT wish to be contacted, please indicate that on the first survey.</p>
 </section>
 
 <section>
 	<h2>DURATION OF STUDY INVOLVEMENT</h2>
-	<p>By completing additional surveys you will help us understand factors related to health and wellbeing in the long-term. We plan to contact you as more surveys and content become available. We anticipate the study will continue to 2099 and we welcome your involvement for as long as you can take part.</p>
+	<p>We anticipate the study will continue to 2099, and we welcome your involvement for as long as you can take part. The more surveys you complete, the more we will learn about wellbeing and health overtime.</p>
 
 	<h2>VOLUNTARY PARTICIPATION</h2>
 	<p>Your participation is entirely voluntary. Your decision not to participate will not have any negative effect on you or your medical care. You can decide to participate now, but withdraw your consent later and stop being in the study without any loss of benefits or medical care to which you are entitled.</p>
@@ -21,7 +23,7 @@ If you decide to end your participation in this study, you should notify Sandra 
 
 <section>
 	<h2>POTENTIAL BENEFITS</h2>       
-	<p>The goal of the WELL for Life Program is to learn how to promote living well. We cannot and do not guarantee or promise you will receive any benefits from this study. All participants will receive information about living well and updates about the research study. By participating, you also will contribute to the health and wellbeing of your community.</p>
+	<p>The goal of the WELL for Life Initiative is to learn how to promote living well. We cannot and do not guarantee or promise you will receive any benefits from this study. All participants will receive information about living well and updates about the research study. By participating, you also will contribute to the health and wellbeing of your community.</p>
 
 	<h2>POSSIBLE RISKS</h2>
 	<p>The risks associated with this study are minimal and we do not expect any negative effects from participating. We follow federal, state, and Stanford University regulations to protect your information. If some of the survey questions make you feel uneasy, you can choose to skip them. While loss of confidentiality is a risk, we take great care to protect your information.</p>
@@ -31,9 +33,6 @@ If you decide to end your participation in this study, you should notify Sandra 
 	<h2>FINANCIAL CONSIDERATIONS</h2>
 	<h3>Payment and Costs</h3>
 	<p>You will not be paid for participating in this study. Program-branded items, however, may be provided during your time in the study such as pens, folders, clothing items, etc. There is no cost to you for participating in this study, other than the time it takes you to complete the surveys.</p>
-
-	<h2>PARTICIPATING IN FUTURE STUDIES</h2>
-	<p>We would like to contact you about participating in other non-WELL related studies in the future.  If you do NOT wish to be contacted, please check the box at the end of the survey.</p>
 </section>
 
 <section>
@@ -48,6 +47,7 @@ If you decide to end your participation in this study, you should notify Sandra 
 
 	<br><br>
 	<addr>
+	Stanford University<br>
 	3000 El Camino Real<br>
 	5 Palo Alto Square, 4th Floor<br>
 	Palo Alto, CA 94306
@@ -56,7 +56,7 @@ If you decide to end your participation in this study, you should notify Sandra 
 
 <section>
 	<h1>Authorization To Use Your Health Information For Research Purposes</h1>
-	<p>Because information about you and your health is personal and private, it generally cannot be used in this research study without your authorization. Clicking on the button that says “I agree” will provide that authorization. The information provided here is intended to inform you about how your health information will be used or disclosed in the study. Your information will only be used in accordance with this authorization and the informed consent form and as required or allowed by law. Please read it carefully before agreeing to participate.</p>
+	<p>Because information about you and your health is personal and private, it generally cannot be used in this research study without your authorization. Clicking on the button that says “I agree to participate in this study” will provide that authorization. The information provided here is intended to inform you about how your health information will be used or disclosed in the study. Your information will only be used in accordance with this authorization and the informed consent form and as required or allowed by law. Please read it carefully before agreeing to participate.</p>
 
 	<h3>What is the purpose of this research study and how will my health information be utilized in the study?</h3>
 	<p>The WELL for Life Registry is an international registry created by the Stanford Prevention Research Center in the Stanford University School of Medicine. The WELL for Life Registry will provide an ongoing source of valuable information on factors related to health and wellness. Thousands of people from all over the world are anticipated to take part in the WELL for Life Registry.</p>
@@ -70,11 +70,11 @@ If you decide to end your participation in this study, you should notify Sandra 
 	<h3>What Personal Information Will Be Obtained, Used or Disclosed?</h3>
 	<p>Your health information related to this study, may be used or disclosed in connection with this research study, including, but not limited to the following protected health information:</p>
 	<ol>
-	<li>Contact information (for example, name, address, phone number, e-mail address, contact details of a friend/family member, Web Uniform Resource Locators (URLs), Internet Protocol (IP) address numbers</li>
-	<li>Socio-demographic data (for example, age, gender, place of birth, ethnicity, race, language, marital status, number of children, number of people in household, education, income, employment status)</li> 
-	<li>Health information (self-reported health, number of prescribed medications, self-reported height and weight, brief health history)</li>
-	<li>Health behavior information (relating to physical activity, sleep, tobacco and alcohol use, diet)</li>
-	<li>Your perceptions (for example of your wellbeing and mood and your social and neighborhood environment)</li>
+	<li>Contact information (e.g., name, address, phone number, e-mail address, contact details of a friend/family member, Web Uniform Resource Locators (URLs), Internet Protocol (IP) address numbers</li>
+	<li>Socio-demographic data (e.g., age, gender, place of birth, ethnicity, race, language, marital status, number of children, number of people in household, education, income, employment status)</li> 
+	<li>Health information (e.g., self-reported health, number of prescribed medications, self-reported height and weight, brief health history)</li>
+	<li>Health behavior information (e.g., relating to physical activity, sleep, tobacco and alcohol use, diet)</li>
+	<li>Perceptual information (e.g., your wellbeing and mood and your social and neighborhood environment)</li>
 	<li>Biometric identifiers, including finger, retinal and voice prints</li>
 	<li>Full face photographic images and any comparable images</li>
 	</ol>
@@ -91,7 +91,7 @@ If you decide to end your participation in this study, you should notify Sandra 
 	<p>The parties listed in the preceding paragraph may disclose your health information to the following persons and organizations for their use in connection with this research study:</p>
 	<ul>
 	<li>The Office for Human Research Protections in the U.S. Department of Health and Human Services</li>
-	<li>Data safety monitoring board, approved collaborators within Stanford University and from other institutions, outside data analysts.</li>
+	<li>Data safety monitoring board, approved collaborators within Stanford University and from other institutions, outside data analysts</li>
 	</ul>
 
 	<p>Your information may be re-disclosed by the recipients described above, if they are not required by law to protect the privacy of the information.</p>
@@ -99,9 +99,6 @@ If you decide to end your participation in this study, you should notify Sandra 
 	<h3>When will my authorization expire?</h3>
 	<p>Your authorization for the use and/or disclosure of your health information will end on 12/31/2099 or when the research project ends, whichever is earlier.</p>
 
-	<h3>Will access to my medical record be limited during the study?</h3>
-	<p>To maintain the integrity of this research study, you may not have access to any health information developed as part of this study until it is completed.  At that point, you would have access to such health information if it was used to make a medical or billing decision about you (e.g., if included in your official medical record).</p> 
-
 	<p>Please print a copy of this page for your records</p>
-	<p>If you wish to be in this study, please click the button that says “I agree”  You will then be directed  to complete the WELL for Life survey.</p>
+	<p>If you wish to be in this study, please click the button that says “I agree”  You will then be directed to complete the first WELL for Life survey.</p>
 </section>

18e3a0a5131f709e109ff72936051ba51de2e7bc
Irvin Szeto <irvins@stanford.edu>
2016-11-28 15:43:59 -0800

    spanish consent language

--- models/inc/well_consent_doc_ss.php
+++ models/inc/well_consent_doc_ss.php
@@ -1,11 +1,11 @@
-<section>
+<section class="lang en">
 	<h2>PURPOSE OF RESEARCH</h2>
 	<p>The WELL for Life Initiative aims to identify factors that contribute to wellbeing and health. The WELL for Life Initiative has been created by the Stanford Prevention Research Center in the Stanford University School of Medicine.</p> 
 	<p>You are being invited to enter the WELL for Life Registry, which is gathering information on people’s health and wellbeing. As a WELL for Life Registry participant you will be invited to take part in future WELL for Life Projects. Our goal is to enroll 10,000 people living in the San Francisco Bay Area. In the future, we hope to enroll participants from all across the US.</p>
 	<p>Your participation in this study is voluntary. If you decide to end your participation in this study, you should notify John Ioannidis at (650) 725-5465 or Sandra Winter at 650-723-8513.</p> 
 </section>
 
-<section>
+<section class="lang en">
 	<h2>PROGRAM ACTIVITIES</h2> 
 	<p>The WELL for Life Registry consists of a number of online surveys. The first survey takes about 20-30 minutes and asks for your contact details and about your basic demographics (e.g., age, gender, language), lifestyle habits and behaviors (e.g., diet, physical activity, tobacco use), and your community. Later surveys will ask similar questions about your lifestyle habits, health, and wellness. You also may be asked to provide a photograph, voice or video recording as a measure of wellbeing. You have the right to refuse to answer particular questions.</p>
 	<p>We will test different strategies to make participation in the WELL for Life Registry engaging. For example, we may “gamify” the survey, provide you with varying types of feedback, change the order in which you answer the surveys or the messages and reminders that we send you, and provide you with informational content.</p>
@@ -13,7 +13,7 @@
 	<p>You may also be asked if you would be interested in being contacted about participating in other non-WELL related studies in the future.  If you do NOT wish to be contacted, please indicate that on the first survey.</p>
 </section>
 
-<section>
+<section class="lang en">
 	<h2>DURATION OF STUDY INVOLVEMENT</h2>
 	<p>We anticipate the study will continue to 2099, and we welcome your involvement for as long as you can take part. The more surveys you complete, the more we will learn about wellbeing and health overtime.</p>
 
@@ -21,7 +21,7 @@
 	<p>Your participation is entirely voluntary. Your decision not to participate will not have any negative effect on you or your medical care. You can decide to participate now, but withdraw your consent later and stop being in the study without any loss of benefits or medical care to which you are entitled.</p>
 </section>
 
-<section>
+<section class="lang en">
 	<h2>POTENTIAL BENEFITS</h2>       
 	<p>The goal of the WELL for Life Initiative is to learn how to promote living well. We cannot and do not guarantee or promise you will receive any benefits from this study. All participants will receive information about living well and updates about the research study. By participating, you also will contribute to the health and wellbeing of your community.</p>
 
@@ -29,18 +29,18 @@
 	<p>The risks associated with this study are minimal and we do not expect any negative effects from participating. We follow federal, state, and Stanford University regulations to protect your information. If some of the survey questions make you feel uneasy, you can choose to skip them. While loss of confidentiality is a risk, we take great care to protect your information.</p>
 </section>
 
-<section> 
+<section class="lang en"> 
 	<h2>FINANCIAL CONSIDERATIONS</h2>
 	<h3>Payment and Costs</h3>
 	<p>You will not be paid for participating in this study. Program-branded items, however, may be provided during your time in the study such as pens, folders, clothing items, etc. There is no cost to you for participating in this study, other than the time it takes you to complete the surveys.</p>
 </section>
 
-<section>
+<section class="lang en">
 	<h2>PARTICIPANT'S RIGHTS</h2>
 	<p>If you have read this form and have decided to participate in this project, please understand your participation is voluntary and you have the right to withdraw your consent or discontinue participation at any time without penalty or loss of benefits to which you are otherwise entitled. The alternative is not to participate.  The results of this research study may be presented at scientific or professional meetings or published in scientific journals.  However, your identity will not be disclosed. You have the right to refuse to answer particular questions.</p> 
 </section>
 
-<section>
+<section class="lang en">
 	<h2>FOR QUESTIONS ABOUT THE STUDY</h2>
 	<p>Questions, Concerns, or Complaints: If you have any questions, concerns or complaints about this research study, its procedures, risks and benefits, you should ask the Protocol Director, John Ioannidis at (650) 725-5465 or the Protocol Co-Director, Sandra Winter at 650-723-8513.</p>
 	<p>Independent Contact: If you are not satisfied with how this study is being conducted, or if you have any concerns, complaints, or general questions about the research or your rights as a participant, please contact the Stanford Institutional Review Board (IRB) to speak to someone independent of the research team at (650)-723-5244 or toll free at 1-866-680-2906. You can also write to the Stanford IRB at:
@@ -54,7 +54,7 @@
 	</addr></p>
 </section>
 
-<section>
+<section class="lang en">
 	<h1>Authorization To Use Your Health Information For Research Purposes</h1>
 	<p>Because information about you and your health is personal and private, it generally cannot be used in this research study without your authorization. Clicking on the button that says “I agree to participate in this study” will provide that authorization. The information provided here is intended to inform you about how your health information will be used or disclosed in the study. Your information will only be used in accordance with this authorization and the informed consent form and as required or allowed by law. Please read it carefully before agreeing to participate.</p>
 
@@ -102,3 +102,110 @@
 	<p>Please print a copy of this page for your records</p>
 	<p>If you wish to be in this study, please click the button that says “I agree”  You will then be directed to complete the first WELL for Life survey.</p>
 </section>
+
+<section class="lang sp">
+	<h2>PROPOSITO DE INVESTIGACION</h2>
+	<p>La Iniciativa de WELL Para Vida tiene como meta identificar factores que contribuyen al bienestar y la salud. La Iniciativa de WELL Para Vida ha sido creada por el Centro de Investigación de Prevención de Stanford en la escuela de medicina de Stanford.</p> 
+	<p>Usted ha sido invitado(a) a ser parte del registro de WELL Para Vida, cual está recolectando información sobre la salud y el bienestar. Como participante de WELL Para Vida, usted será invitado(a) a participar en los futuros proyectos de WELL Para Vida. Nuestra meta es inscribir participantes de todas partes de los Estados Unidos.</p>
+	<p>Su participación en este estudio es voluntaria. Si decide ceder su participación en este estudio, debería notificar a John Ioannidis a 650-725-5465 o Sandra Winter a 650-723-8513.</p> 
+</section>
+
+<section class="lang sp">
+	<h2>ACTIVIDADES DEL PROGRAMA</h2> 
+	<p>El Registro de WELL Para Vida consiste de un número de encuestas computadorizadas. La primera encuesta tomará alrededor de 20-30 minutos y le pide sus detalles de contacto y sobre detalles demográficos básicos (ej. Edad, género, lenguaje), hábitos y estilo de vida, y su comunidad. Después, las encuestas incluirán preguntas similares sobre su estilo de vida, salud, y bienestar. También puede ser que le pediremos que provea una foto o grabación de video. Usted tiene el derecho de decidir a no contestar preguntas especificas.</p>
+	<p>Intentaremos diferentes estrategias para convertir su experiencia con WELL Para Vida en una experiencia entretenida. Por ejemplo, algunas encuestas podrían ser como “juegos,” podríamos proveerle diferentes tipos de sugerencias, cambiar el orden en que contesta las encuestas o los mensajes que le enviamos, y proveerle contenido informacional.</p>
+	<p>Le podríamos preguntar si está interesado(a) en ser entrevistado(a) sobre su experiencia como participante de WELL Para Vida. Si NO desea ser contactado(a) para ser entrevistado(a), por favor indíquelo en la primera encuesta.</p>
+	<p>También le podríamos preguntar si está interesado(a) en ser contactado(a) para participar en otros estudios fuera de WELL en el futuro. Si NO desea ser contactado(a), por favor indíquelo en la encuesta.</p>
+	<p>Le podríamos pedir su permiso para incluir su nombre, detalles de contacto, y respuestas de la primera encuesta que complete en el Registro de Stanford. Esto es un registro de individuos que están de acuerdo a ser contactados en el futuro para aprender más sobre estudios investigativos en Stanford. Esto no le obliga a participar en estos estudios. Si NO desea ser contactado(a), por favor indíquelo en la primera encuesta.</p>
+</section>
+
+<section class="lang sp">
+	<h2>DURACION DE SU INVOLUCRAMIENTO CON EL ESTUDIO</h2>
+	<p>Anticipamos que este estudio continuará hasta 2099 y usted puede involucrarse con este proyecto por cuanto tiempo desee. Mientras más encuestas complete, más aprenderemos sobre su bienestar y salud a través del tiempo.</p>
+
+	<h2>PARTICIPACION VOLUNTARIA</h2>
+	<p>Su participación es completamente voluntaria. Su decisión de no participar no tendrá ningún impacto en usted o su cuido médico. Usted puede decidir a participar ahora, pero luego retirar su consentimiento y parar de ser parte del estudio a ningún costo a sus beneficios o salud médica.</p>
+</section>
+
+<section class="lang sp">
+	<h2>POSIBLES BENEFICIOS</h2>       
+	<p>La meta del Programa de Investigación de Vivir Bien es aprender cómo promover vivir bien. No podemos garantizar o prometer que recibirá algún beneficio de este estudio. Todos los participantes recibirán información sobre vivir bien y también recibirán información continua sobre el estudio investigativo. Al participar, usted también va a contribuir a la salud y el bienestar de su comunidad.</p>
+
+	<h2>RIESGOS POSIBLES</h2>
+	<p>Los riesgos asociados con este estudio son mínimos y no esperamos ningunos efectos negativos relacionados a su participación. Nosotros seguimos las regulaciones y protocolo federales, estatales, y de la Universidad de Stanford para proteger su información. Si algunas preguntas de la encuesta le hacen sentirse incómodo(a), usted puede elegir saltar esas preguntas. Mientras la pérdida de confidencialidad es un riesgo, nosotros nos esforzamos para proteger su información.</p>
+</section>
+
+<section class="lang sp"> 
+	<h2>CONSIDERACIONES FINANCIERAS</h2>
+	<h3>Payment and Costs</h3>
+	<p>No será pagado por participar en este estudio. Sin embargo, puede ser que le proveeremos objetos con el logo del programa, tal como bolígrafos, camisas, cartapacios, etc. No hay ningún costo para participar en este estudio, sólo el tiempo que le tome completar las encuestas.</p>
+</section>
+
+<section class="lang sp">
+	<h2>DERECHOS DEL PARTICIPANTE</h2>
+	<p>Si ha leído este formulario y ha decidido participar en este proyecto, por favor entienda que su participación es voluntaria y usted tiene el derecho de darse de baja o descontinuar su participación a cualquier momento sin penalidad o pérdida de beneficios. La alternativa es no participar. Los resultados de este estudio pueden ser presentados en reuniones científicas o profesionales o publicados en revistas científicas. No obstante, su identidad no será revelada. Usted tiene el derecho a no contestar preguntas específicas.</p> 
+</section>
+
+<section class="lang sp">
+	<h2>PARA PREGUNTAS SOBRE ESTE ESTUDIO, FAVOR CONTACTAR</h2>
+	<p>Preguntas, preocupaciones o quejas: Si tiene algunas preguntas, preocupaciones, o quejas sobre este estudio, los procedimientos, riesgos y beneficios, debería hablar con el director del protocolo, John Ioannidis. Puede ponerse en comunicación ahora o después a 650-725-6465. También puede contactar a la co-directora, Sandra Winter a 650-723-8513.</p>
+	<p>Si no esta satisfecho(a) con la manera en que este estudio esta siendo conducido, o si tiene algunas preocupaciones, quejas o preguntas generales sobre la investigación o sus derechos como participante, favor contactar el Institutional Review Board (IRB) de Stanford para hablar con alguien independiente del equipo investigativo al (650) 723-5244 o sin costo al 1-866-680-2906. También puede escribirle al:
+
+	<br><br>
+	<addr>
+	Stanford University<br>
+	3000 El Camino Real<br>
+	5 Palo Alto Square, 4th Floor<br>
+	Palo Alto, CA 94306
+	</addr></p>
+</section>
+
+<section class="lang sp">
+	<h1>Autorización Para Usar Su Información de Salud Para Propósitos de Investigación</h1>
+	<p>La información sobre su salud es personal y privada, y no puede ser utilizada en esta investigación sin su autorización. Seleccionar el botón que diga “Estoy de acuerdo a participar en este estudio” proveerá su autorización. La información proveída aquí le informará sobre cómo su información de salud será utilizada o revelada en este estudio. Su información solamente será utilizada en acorde con esta autorización y el consentimiento informado y como requerido o permitido por la ley. Por favor lea cuidadosamente antes de conceder a participar.</p>
+
+	<h3>¿Cual es el propósito de esta investigación y cómo utilizarán mi información de salud?</h3>
+	<p>El Registro de Vivir Bien es un registro internacional creado por el Centro de Investigación de Prevención de Stanford en la escuela de medicina de Stanford. El Registro de Vivir Bien será un recurso de información valiosa sobre factores relacionados a la salud y el bienestar. Miles de personas de todas partes del mundo serán parte del Registro de Vivir Bien.</p>
+
+	<h3>¿Tengo que proveer mi autorización?</h3>
+	<p>No tiene que proveer su autorización. Pero si no la provee, no podrá participar en esta investigación. Proveer su autorización no es una condición para recibir cuido médico fuera de este estudio.</p>
+
+	<p>¿Si proveo mi autorización, puedo revocarla o salirme del estudio más tarde?</p>
+	<p>Si decide participar, puede retirar su autorización para el uso de su información de salud (y descontinuar cualquier otra participación en el estudio) a cualquier momento. Después de la revocación, su información de salud no será utilizada o revelada en el estudio, solo lo que la ley nos permita y sea necesario para mantener la integridad de la investigación. Si desea revocar su autorización para el uso investigativo o el uso de su información de salud en este estudio, tiene que escribirle a la directora del protocolo: Sandra Winter, Stanford Prevention Research Center, Department of Medicine, Stanford University, Medical School Office Building, X3C02. 1265 Welch Road. Stanford, CA 94305-5411 o comuníquese con sjwinter@stanford.edu.</p>
+
+	<h3>¿Qué información personal será obtenida, utilizada, o revelada?</h3>
+	<p>Su información de salud relacionada con este estudio puede ser utilizada o revelada en conexión con este proyecto incluyendo, pero no limitado, a la siguiente información de salud:</p>
+	<ol>
+	<li>Información de contacto (por ejemplo, nombre, dirección, número de teléfono, correo electrónico, información de contacto de amistades/familiares, URLs, dirección de IP.</li>
+	<li>Data socio-económica (por ejemplo, edad, género, lugar de nacimiento, etnicidad, lenguaje, estatus matrimonial, número de hijos, número de personas en el hogar, educación, ingreso, empleo)</li> 
+	<li>Información de salud (salud reportada, número de medicaciones, altura y peso, historial de salud)</li>
+	<li>Comportamiento de salud (relacionado a actividad física, dormir, uso de tabaco y alcohol, dieta)</li>
+	<li>Sus percepciones (por ejemplo, de su bienestar y emociones y su ambiente social y comunitario)</li>
+	<li>Identificadores biométricos, incluyendo muestras de huellas dactilares, retina y voz</li>
+	<li>Imágenes fotográficas de su cara e imágenes comparables</li>
+	</ol>
+
+	<h3>¿Quién puede utilizar o revelar esta información?</h3>
+	<p>Los siguientes partidos están autorizados para utilizar y revelar su información de salud en conexión con este estudio investigativo:</p>
+	<ul>
+	<li>Directora de Protocolo (Sandra Winter, PhD)</li>
+	<li>Panel Administrativo de Participantes Humanos en Investigaciones Médicas de Stanford y cualquier otra unidad de Stanford como sea necesario.</li>
+	<li>Empleados de la investigación</li>
+	</ul>
+
+	<h3>¿Quién puede recibir o utilizar esta información?</h3>
+	<p>Los partidos listados en el párrafo antecedente pueden revelar su información de salud a las siguientes personas y organizaciones para su uso en conexión con este estudio investigativo:</p>
+	<ul>
+	<li>La Oficina de Protecciones de Investigaciones Humanas en el Departamento de Salud y Servicios Humanos de Estados Unidos</li>
+	<li>Junta para la seguridad y el monitoreo de data, colaboradores en otras instituciones, analistas de data externos, el Instituto Nacional de Salud</li>
+	</ul>
+
+	<p>Su información puede ser revelada por los recipientes descritos previamente, si no están requeridos por ley a proteger la privacidad de la información.</p>
+
+	<h3>¿Cuando expirará mi autorización?</h3>
+	<p>Su autorización para el uso o la revelación de su información de salud terminará en 12/31/2099 o cuando termine el proyecto, cualquiera venga primero.</p>
+
+	<p>Por favor imprima una copia de esta página para sus records</p>
+	<p>Si desea ser parte del estudio, por favor seleccione el botón que diga “Estoy de acuerdo a participar.” Entonces será dirigido a completar la encuesta de WELL.</p>
+</section>
+

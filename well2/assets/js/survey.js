c9a5ccedff95d7065bf897093eee750702546178
Irvin Szeto <irvins@stanford.edu>
2018-01-18 16:23:56 -0800

    fixed the multiple sleep results on the page

--- well2/assets/js/survey.js
+++ well2/assets/js/survey.js
@@ -778,9 +778,18 @@ function showSleepScoring(){
     data: "&sleep=" + JSON.stringify(all_answers),
     success:function(result){
       var sleepTitle    = $("#checkmutation h2").clone();
-      $("#checkmutation").find("h2","#psqi_results").not(".submits").remove();
-      $("#checkmutation").prepend(sleepTitle);
-      nextSection.find("h2").after(result);
+      
+      if($("#psqi_display_results").length){
+        $("#psqi_display_results").empty();
+        $("#psqi_display_results").append(result);
+      }else{
+        var psqi_container = $("<div id='psqi_display_results'>");
+        nextSection.find("h2").after(psqi_container);
+      }
+      // $("#checkmutation").find("h2","#psqi_results").not(".submits").remove();
+      // $("#checkmutation").prepend(sleepTitle);
+      // $("#checkmutation #psqi_results").remove();
+      // nextSection.find("h2").after(result);
 
       var PSQI_SCORE      = $("#psqi_score").text();
       var dataURL         = "survey.php?sleep=1";
